e72605e879d81db7cc7cc4949acbc50c4fd87822
kazuya kawaguchi <kawakazu80@gmail.com>
2025-06-30 14:56:32 +0900

    fix: drop locales on gunshi package

--- packages/gunshi/tsdown.config.ts
+++ packages/gunshi/tsdown.config.ts
@@ -1,26 +1,5 @@
 import { lintJsrExports } from 'jsr-exports-lint/tsdown'
-import fs from 'node:fs'
-import path from 'node:path'
 import { defineConfig } from 'tsdown'
-
-const dirname = import.meta.dirname
-
-function getDistributionableLocaleFiles() {
-  const localesDir = path.join(dirname, './src/locales')
-  const outDir = path.join(dirname, 'lib', 'locales')
-  return fs.readdirSync(localesDir).reduce(
-    (acc, file) => {
-      if (file.endsWith('.json')) {
-        acc.push({
-          from: path.join(localesDir, file),
-          to: path.join(outDir, file)
-        })
-      }
-      return acc
-    },
-    [] as { from: string; to: string }[]
-  )
-}
 
 const config: ReturnType<typeof defineConfig> = defineConfig({
   entry: [
@@ -38,8 +17,7 @@ const config: ReturnType<typeof defineConfig> = defineConfig({
   dts: true,
   hooks: {
     'build:done': lintJsrExports()
-  },
-  copy: [...getDistributionableLocaleFiles()]
+  }
 })
 
 export default config
