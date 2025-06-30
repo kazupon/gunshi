588aadff43af46e82115b25830f5219c272b6710
kazuya kawaguchi <kawakazu80@gmail.com>
2025-06-30 17:22:09 +0900

    fix: add `@gunshi/bone` package

--- packages/gunshi/src/bone.ts
+++ packages/gunshi/src/bone.ts
@@ -5,9 +5,31 @@
  * - `cli`: The main CLI function to run the command, **not included `global` and `renderer` built-in plugins**.
  * - some basic type definitions only.
  *
+ * > [!IMPORTANT]
+ * > You cannot use the `cli` function in this entry to display the usage of the command with `--help` option.
+ * > This entry point is provided to allow users to customize it completely, such as command usage rendering and plugin composition.
+ *
  * @example
  * ```js
  * import { cli } from 'gunshi/bone'
+ * import global from '@gunshi/plugin-global'
+ * import renderer from '@gunshi/plugin-renderer'
+ * import i18n from '@gunshi/plugin-i18n'
+ *
+ * const entry = (ctx) => {
+ *   // entry logic ...
+ * }
+ *
+ * await cli(process.argv.slice(2), entry, {
+ *   // ...
+ *   plugins: [
+ *     global(),
+ *     renderer(),
+ *     i18n({
+ *      // plugin options ...
+ *     })
+ *   ],
+ * })
  * ```
  *
  * @module bone
