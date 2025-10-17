c80f5d919f9619db4fa5123b575be35af4b21789
kazuya kawaguchi <kawakazu80@gmail.com>
2025-10-17 23:43:26 +0900

    fix

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout: 60_000,
     include: ['**/*.spec.?(c|m)[jt]s?(x)']
   }
 })
