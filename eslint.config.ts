a587fb5d87f2fdaa3eb55aa2fdb692cd6cfe9b61
kazuya kawaguchi <kawakazu80@gmail.com>
2025-12-10 15:01:56 +0900

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -46,7 +46,6 @@ const config: ReturnType<typeof defineConfig> = defineConfig(
     typescript: 'syntax',
     ignores: [
       './playground/**',
-      './packages/plugin-completion/src/bombshell/*.ts', // NOTE(kazupon): bombshell code is forked, so ignore.
       './packages/plugin-i18n/test/*.ts', // NOTE(kazupon): test codes
       './packages/gunshi/test/*.ts' // NOTE(kazupon): test codes
     ]
@@ -106,51 +105,7 @@ const config: ReturnType<typeof defineConfig> = defineConfig(
       'import/no-named-as-default': 'off',
       'import/no-named-as-default-member': 'off',
       'import/namespace': 'off',
-      'import/no-duplicates': 'off',
-      'no-unused-labels': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      // eslint-plugin-markdown-preferences rules
-      'markdown-preferences/canonical-code-block-language': 'error',
-      'markdown-preferences/emoji-notation': 'error',
-      // 'markdown-preferences/heading-casing': 'error',
-      'markdown-preferences/ordered-list-marker-start': 'error',
-      // 'markdown-preferences/prefer-inline-code-words': 'error'
-      // 'markdown-preferences/prefer-linked-words': 'error'
-      'markdown-preferences/table-header-casing': 'error',
-      'markdown-preferences/bullet-list-marker-style': 'error',
-      'markdown-preferences/code-fence-style': 'error',
-      'markdown-preferences/emphasis-delimiters-style': 'error',
-      'markdown-preferences/level1-heading-style': 'error',
-      'markdown-preferences/level2-heading-style': 'error',
-      'markdown-preferences/link-destination-style': 'error',
-      'markdown-preferences/link-title-style': 'error',
-      'markdown-preferences/ordered-list-marker-style': 'error',
-      // 'markdown-preferences/prefer-link-reference-definitions': 'error'
-      'markdown-preferences/strikethrough-delimiters-style': 'error',
-      'markdown-preferences/thematic-break-character-style': 'error',
-      'markdown-preferences/code-fence-spacing': 'error',
-      'markdown-preferences/custom-container-marker-spacing': 'error',
-      // 'markdown-preferences/indent': 'error',
-      'markdown-preferences/link-bracket-newline': 'error',
-      'markdown-preferences/link-bracket-spacing': 'error',
-      'markdown-preferences/link-paren-newline': 'error',
-      'markdown-preferences/link-paren-spacing': 'error',
-      'markdown-preferences/no-multi-spaces': 'error',
-      'markdown-preferences/no-multiple-empty-lines': 'error',
-      'markdown-preferences/no-trailing-spaces': 'error',
-      'markdown-preferences/padded-custom-containers': 'error',
-      'markdown-preferences/padding-line-between-blocks': 'error',
-      'markdown-preferences/table-pipe-spacing': 'error',
-      'markdown-preferences/atx-heading-closing-sequence-length': 'error',
-      'markdown-preferences/atx-heading-closing-sequence': 'error',
-      'markdown-preferences/code-fence-length': 'error',
-      'markdown-preferences/ordered-list-marker-sequence': 'error',
-      'markdown-preferences/setext-heading-underline-length': 'error',
-      // 'markdown-preferences/sort-definitions': 'error'
-      'markdown-preferences/table-leading-trailing-pipes': 'error',
-      'markdown-preferences/table-pipe-alignment': 'error',
-      'markdown-preferences/thematic-break-length': 'error',
-      'markdown-preferences/thematic-break-sequence-pattern': 'error'
+      'import/no-duplicates': 'off'
     }
   }),
   vitest(),
