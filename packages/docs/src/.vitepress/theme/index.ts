bb6abde0a7265e0883f12965a5aa51fb27523d3b
kazuya kawaguchi <kawakazu80@gmail.com>
2026-02-02 19:40:51 +0900

    fix(gunshi) zero dependency (#492)

--- packages/docs/src/.vitepress/theme/index.ts
+++ packages/docs/src/.vitepress/theme/index.ts
@@ -1,15 +1,16 @@
+import 'virtual:group-icons.css'
 import Theme from 'vitepress/theme'
 import { defineAsyncComponent, h } from 'vue'
+// @ts-expect-error -- FIXME: missing types
 import HomeSponsors from './components/HomeSponsors.vue'
 import './custom.css'
-// eslint-disable-next-line import/no-unresolved -- NOTE(kazupon): virtual module
-import 'virtual:group-icons.css'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
       'home-features-after': () => h(HomeSponsors),
+      // @ts-expect-error -- FIXME: missing types
       'layout-top': () => h(defineAsyncComponent(() => import('./components/GunshiBanner.vue')))
     })
   },
